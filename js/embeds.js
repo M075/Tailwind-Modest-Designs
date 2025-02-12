8d7f53b81ddb10d142f6203b7451f9f7567a9c7e
M075 <moemotara@gmail.com>
2025-02-12 21:49:38 +0200

    asdf

--- js/embeds.js
+++ js/embeds.js
@@ -1,30 +1,30 @@
   // Load footer.html into the placeholder
-  fetch('/components/footer.html')
+  fetch('../components/footer.html')
   .then(response => response.text())
   .then(html => {
     document.getElementById('footer-placeholder').innerHTML = html;
   });
 
    // Load nav into the placeholder
-   fetch('/components/navbar.html')
+   fetch('../components/navbar.html')
    .then(response => response.text())
    .then(html => {
      document.getElementById('header-placeholder').innerHTML = html;
    });
 
-   fetch('/components/addon-prices.html')
+   fetch('../components/addon-prices.html')
    .then(response => response.text())
    .then(html => {
      document.getElementById('addon-prices').innerHTML = html;
    });
 
-    fetch('/components/feature-comparison.html')
+    fetch('../components/feature-comparison.html')
       .then(response => response.text())
       .then(html => {
         document.getElementById('feature-comparison').innerHTML = html;
       });
 
-      fetch('/components/ecomm-feature-comparison.html')
+      fetch('../components/ecomm-feature-comparison.html')
       .then(response => response.text())
       .then(html => {
         document.getElementById('ecomm-feature-comparison').innerHTML = html;
